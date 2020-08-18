7831074fdc654311dd231ca03f1b8888419e3c94
Sam Winslow <sammywinslow@gmail.com>
2020-08-18 15:07:26 -0400

    connects to backend, speaks whole document

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import React from 'react'
 import annyang from 'annyang'
 import { HostedModel } from '@runwayml/hosted-models'
 import './App.css';
+import axios from 'axios'
 
 const commands = {
   'hello': () => console.log('called')
 }
-
 const model = new HostedModel({
   url: 'https://sam-essays-1.hosted-models.runwayml.cloud/v1/',
   token: 'Pb1p+Jfd0VPzBzFmgajitg=='
 })
+const speech = axios.create()
+speech.defaults.baseURL = "https://w8ylvqkryk.execute-api.us-east-1.amazonaws.com/dev/"
+speech.defaults.timeout = 0
 
 class App extends React.Component {
   state = {
@@ -20,14 +23,33 @@ class App extends React.Component {
   queryModel = (prompt = '') => {
     console.log('called')
     model.query({
-      prompt: this.state.message + prompt,
+      prompt: this.state.message + prompt + ' ',
       max_characters: 140
     }).then((result) => {
       console.log(result)
+      let message = result.generated_text.substring(0, result.generated_text.lastIndexOf(' ')) + ' '
       this.setState({
-        message: result.generated_text
+        message
       })
+      this.speakMessage()
     })
+  }
+
+  speakMessage = async () => {
+    const { message } = this.state
+    const { data } = await speech.post('/speak', {
+      text: message,
+      voice: 'Matthew'
+    })
+    if (data.url) {
+      annyang.abort()
+      let audio = new Audio(data.url)
+      audio.addEventListener('ended', () => {
+        annyang.resume()
+      })
+      audio.play()
+    }
+    console.log(data)
   }
 
   componentDidMount() {
@@ -35,7 +57,7 @@ class App extends React.Component {
     annyang.addCommands(commands)
     annyang.start()
     annyang.addCallback('result', (phrases) => {
-      let prompt = phrases[0] + ' '
+      let prompt = phrases[0]
       // this.setState({
       //   message: this.state.message + prompt
       // })
@@ -46,6 +68,9 @@ class App extends React.Component {
     const { message } = this.state
     return (
       <div className="App">
+        <button onClick={() => this.queryModel()}>Submit</button>
+        <button onClick={() => this.speakMessage()}>Speak</button>
+        <br />
         <p>{message}</p>
       </div>
     )
