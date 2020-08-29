51d4e2e51ea73b85d742f2650b775587378ca0ce
Sam Winslow <sammywinslow@gmail.com>
2020-08-29 15:20:05 -0400

    Fix secrets importing

--- src/config/Api.js
+++ src/config/Api.js
@@ -1,20 +1,16 @@
 import { HostedModel } from '@runwayml/hosted-models'
 import axios from 'axios'
-import RUNWAY_KEYS from './secrets.json'
+import * as secrets from './secrets.json'
 
-console.log(RUNWAY_KEYS)
 const model = new HostedModel({
-  url: RUNWAY_KEYS.url,
-  token: RUNWAY_KEYS.token
+  url: secrets.RUNWAY_URL,
+  token: secrets.RUNWAY_TOKEN
 })
 const server = axios.create()
 server.defaults.baseURL = "https://w8ylvqkryk.execute-api.us-east-1.amazonaws.com/dev/"
 server.defaults.timeout = 0
 
 class Api {
-  constructor() {
-    console.log(RUNWAY_KEYS)
-  }
   queryModel = async (prompt, max_characters) => {
     return model.query({
       prompt,
