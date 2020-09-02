9f6d86f1438cf6d46fb456e2756a5e032d66df7e
Sam Winslow <sammywinslow@gmail.com>
2020-09-02 17:18:28 -0400

    Delete button styling

--- src/components/DocumentList.js
+++ src/components/DocumentList.js
@@ -1,25 +1,44 @@
 import React from 'react'
 import styled from 'styled-components'
 import theme from '../constants/theme'
-import { Menu, PlusIcon } from 'evergreen-ui'
+import {
+  Menu,
+  PlusIcon,
+  IconButton,
+  TrashIcon
+} from 'evergreen-ui'
 
 const ListItem = styled(Menu.Item)`
-  width: 14rem;
+  width: 100%;
   cursor: pointer;
   border-radius: 0.25rem;
+  .trashButton {
+    display: none;
+  }
   &:hover {
     background-color: ${theme.colors.accentDarker};
+    .trashButton {
+      display: flex;
+    }
   }
   &:focus {
     background-color: ${theme.colors.accentDarker};
     border: none;
     outline: none;
+    .trashButton {
+      display: flex;
+    }
+  }
+  span {
+    width: 100% !important;
+    display: flex;
+    justify-content: space-between;
   }
 `
 const ListItemTitle = styled.div`
   font-family: ${theme.type.base.fontFamily};
   font-size: 1rem;
-  width: 13rem;
+  flex-grow: 1;
   white-space: nowrap;
   overflow: hidden;
   text-overflow: ellipsis;
@@ -32,24 +51,32 @@ class DocumentList extends React.Component {
     const { documents, onChangeSelectedDocument, selectedDocument } = this.props
     return (
       <Menu>
-        <ListItem
-          href={`#${document.noteId}`}
-          onSelect={() => onChangeSelectedDocument('new')}
-          icon={<PlusIcon color={theme.colors.primary} />}
-        >
-          <ListItemTitle
-            primary
-          >New Document</ListItemTitle>
-        </ListItem>
-        {documents.map((document) => (
+        <div style={{ width: '16rem'}}>
           <ListItem
-            key={document.noteId}
             href={`#${document.noteId}`}
-            onSelect={() => onChangeSelectedDocument(document.noteId)}
+            onSelect={() => onChangeSelectedDocument('new')}
+            icon={<PlusIcon color={theme.colors.primary} />}
           >
-            <ListItemTitle active={(document.noteId === selectedDocument)}>{document.title}</ListItemTitle>
+            <ListItemTitle
+              primary
+            >New Document</ListItemTitle>
           </ListItem>
-        ))}
+          {documents.map((document) => (
+            <ListItem
+              key={document.noteId}
+              href={`#${document.noteId}`}
+              onSelect={() => onChangeSelectedDocument(document.noteId)}
+            >
+              <ListItemTitle active={(document.noteId === selectedDocument)}>{document.title || 'Untitled Document'}</ListItemTitle>
+              <IconButton
+                className="trashButton"
+                appearance="minimal"
+                icon={TrashIcon}
+                height={20}
+                onClick={() => console.log('ayo')} />
+            </ListItem>
+          ))}
+        </div>
       </Menu>
     )
   }
